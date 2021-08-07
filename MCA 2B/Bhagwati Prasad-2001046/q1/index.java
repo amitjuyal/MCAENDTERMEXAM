c49a143d025a22469fe190f93fde2b8ca4615e95
Bhagwati Prasad Joshi <48825037+bhagwatiprasad@users.noreply.github.com>
2021-08-07 11:20:17 +0530

    Add files via upload

---
+++ MCA 2B/Bhagwati Prasad-2001046/q1/index.java
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=US-ASCII">
+<title>Login Form - Session Management by Cookies</title>
+</head>
+<body>
+      <form action="loginServlet" method="post">
+            Enter Your Username: <input type="text"  name="userName"><br>
+            Enter Your Password: <input type="password" name="password">
+            <br> <br> <br> <input type="submit" value="Login">
+        </form>
+</body>
+</html>
