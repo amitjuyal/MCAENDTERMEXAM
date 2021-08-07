10e2802c01ff091aa9f3d89b4ce1108191767267
Ga-utam <64944949+Ga-utam@users.noreply.github.com>
2021-08-07 11:31:09 +0530

    Add files via upload

---
+++ MCA 2B/GautamNegi-20711073/ques1.java
@@ -0,0 +1,85 @@
+//Index Html
+<!DOCTYPE html>
+<html>
+<head>
+<title>question one</title>
+</head>
+<body>
+	<form action="login">
+		<label>User name</label> <input type="text" placeholder="enter your username.." 
+		name="login"/> <br/><br/>
+		<label>Password</label> <input type="text" placeholder="enter password..." 
+		name="password"/> <br/><br/>
+		<button type="submit">login</button><br/>
+	</form>
+</body>
+</html>
+
+//Connection
+import java.sql.*;
+
+public class conn {
+    public static boolean checkUser(String email,String pass)
+    {
+        boolean st =false;
+        try {
+        	Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","abcd");
+            PreparedStatement ps = con.prepareStatement("select * from login where username=? and password=?");
+			ps.setString(1, email);
+			ps.setString(2, pass);
+            ResultSet rs =ps.executeQuery();
+            st = rs.next();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return st;
+    }
+}
+
+//Welcome java file
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class welcome extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("Welcome user");
+    }
+}
+
+//Login file
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class Login extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        String username = request.getParameter("username");
+        String pass = request.getParameter("password");
+
+        if(conn.checkUser(username, pass)){
+            RequestDispatcher rs = request.getRequestDispatcher("Welcome");
+            rs.forward(request, response);
+        }
+        else
+        {
+            out.println("Username or Password incorrect");
+            RequestDispatcher rs = request.getRequestDispatcher("index.html");
+            rs.include(request, response);
+        }
+    }
+}
